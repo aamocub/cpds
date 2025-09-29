1b8371af192567b815b39da68d3f79f59169376b
vcastilla <119356332+vcastilla@users.noreply.github.com>
2025-09-29 10:02:01 +0200

    Erl: Update acceptor

--- acceptor.erl
+++ acceptor.erl
@@ -3,9 +3,9 @@
 
 start(Name, PanelId) ->
   spawn(fun() -> init(Name, PanelId) end).
-        
+
 init(Name, PanelId) ->
-  Promised = order:null(), 
+  Promised = order:null(),
   Voted = order:null(),
   Value = na,
   acceptor(Name, Promised, Voted, Value, PanelId).
@@ -13,37 +13,37 @@ init(Name, PanelId) ->
 acceptor(Name, Promised, Voted, Value, PanelId) ->
   receive
     {prepare, Proposer, Round} ->
-      case order:gr(..., ...) of
+      case order:gr(Round, Promise) of
         true ->
-          ... ! {promise, ..., ..., ...},               
+          Proposer ! {promise, Round, Voted, Value},
       io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n",
-                 [Name, ..., ..., Value]),
+                 [Name, Promised, Voted, Value]),
           % Update gui
           Colour = case Value of na -> {0,0,0}; _ -> Value end,
-          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
-                     "Promised: " ++ io_lib:format("~p", [...]), Colour},
-          acceptor(Name, ..., Voted, Value, PanelId);
+          PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]),
+                     "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+          acceptor(Name, Round, Voted, Value, PanelId);
         false ->
-          ... ! {sorry, {prepare, ...}},
-          acceptor(Name, ..., Voted, Value, PanelId)
+          Proposer ! {sorry, {prepare, Round}},
+          acceptor(Name, Promise, Voted, Value, PanelId)
       end;
     {accept, Proposer, Round, Proposal} ->
-      case order:goe(..., ...) of
+      case order:goe(..., ...) of % Can we vote for this ballot?
         true ->
-          ... ! {vote, ...},
-          case order:goe(..., ...) of
+          Proposer ! {vote, Round},
+          case order:goe(..., ...) of % Is the ballot number higher than the current maximum one?
             true ->
       io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n",
-                 [Name, Promised, ..., ...]),
+                 [Name, Promised, Round, Proposal]),
               % Update gui
-              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [...]), 
-                         "Promised: " ++ io_lib:format("~p", [Promised]), ...},
-              acceptor(Name, Promised, ..., ..., PanelId);
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]),
+                         "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              acceptor(Name, Promised, Round, Proposal, PanelId);
             false ->
               acceptor(Name, Promised, ..., ..., PanelId)
-          end;                            
+          end;
         false ->
-          ... ! {sorry, {accept, ...}},
+          Proposer ! {sorry, {accept, Round}},
           acceptor(Name, Promised, Voted, Value, PanelId)
       end;
     stop ->
