0741967b1b6e8c7f7b7e1e51b3689992a7a7337c
Alfonso Amorós Cubells <alfonso.alfurtx@gmail.com>
2025-10-16 17:42:35 +0200

    erl: fix entry code syntax

--- opty/entry.erl
+++ opty/entry.erl
@@ -11,13 +11,13 @@ entry(Value, Time) ->
     receive
         {read, Ref, From} ->
             %% Send ok message back to requester. Tagged with reference, pid, value and timestamp.
-            From ! {{Ref, self(), Value, Time}, ok}
+            From ! {{Ref, self(), Value, Time}, ok},
             entry(Value, Time);
         {write, New} ->
             entry(New , make_ref());  %% Write new value to entry and update timestamp
         {check, Ref, Readtime, From} ->
             if
-                Readtime == Time -> From ! {Ref, ok}
+                Readtime == Time -> From ! {Ref, ok};
                 true -> From ! {Ref, abort}
             end,
             entry(Value, Time);
