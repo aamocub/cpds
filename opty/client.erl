814df628ded1ec5fb16209c8b0d5d1b7a7e6e0f7
vcastilla <119356332+vcastilla@users.noreply.github.com>
2025-10-26 20:33:45 +0100

    Revert 'wip'

--- opty/client.erl
+++ opty/client.erl
@@ -1,11 +1,10 @@
 -module(client).
--export([start/4]).
+-export([start/5]).
 
-start(ClientID, Entries, Server, SubsetLen) ->
-    Subset = lists:sublist([X || {_, X} <- lists:sort([{rand:uniform(), E} || E <- lists:seq(1, Entries)])], SubsetLen),
-    spawn(fun() -> open(ClientID, Subset, Server, 0, 0) end).
+start(ClientID, Entries, Reads, Writes, Server) ->
+    spawn(fun() -> open(ClientID, Entries, Reads, Writes, Server, 0, 0) end).
 
-open(ClientID, Subset, Server, Total, Ok) ->
+open(ClientID, Entries, Reads, Writes, Server, Total, Ok) ->
     Server ! {open, self()},
     receive
         {stop, From} ->
@@ -15,34 +14,42 @@ open(ClientID, Subset, Server, Total, Ok) ->
             ok;
         {transaction, Validator, Store} ->
             Handler = handler:start(self(), Validator, Store),
-            case do_transaction(ClientID, Subset, Handler) of
+            case do_transaction(ClientID, Entries, Reads, Writes, Handler) of
                 ok ->
-                    open(ClientID, Subset, Server, Total+1, Ok+1);
+                    open(ClientID, Entries, Reads, Writes, Server, Total+1, Ok+1);
                 abort ->
-                    open(ClientID, Subset, Server, Total+1, Ok)
+                    open(ClientID, Entries, Reads, Writes, Server, Total+1, Ok)
             end
     end.
 
-do_transaction(_, [], Handler) ->
+do_transaction(_, _, 0, 0, Handler) ->
     do_commit(Handler);
-do_transaction(ClientID, Subset, Handler) ->
-    [Num | RemSubset] = Subset,
+do_transaction(ClientID, Entries, 0, Writes, Handler) ->
+    do_write(Entries, Handler, ClientID),
+    do_transaction(ClientID, Entries, 0, Writes-1, Handler);
+do_transaction(ClientID, Entries, Reads, 0, Handler) ->
+    do_read(Entries, Handler),
+    do_transaction(ClientID, Entries, Reads-1, 0, Handler);
+do_transaction(ClientID, Entries, Reads, Writes, Handler) ->
     Op = rand:uniform(),
     if Op >= 0.5 ->
-         do_read(Num, Handler);
+         do_read(Entries, Handler),
+         do_transaction(ClientID, Entries, Reads-1, Writes, Handler);
        true -> 
-         do_write(Num, Handler, ClientID)
-    end,
-    do_transaction(ClientID, RemSubset, Handler).
+         do_write(Entries, Handler, ClientID),
+         do_transaction(ClientID, Entries, Reads, Writes-1, Handler)
+    end.
 
-do_read(Num, Handler) ->
+do_read(Entries, Handler) ->
     Ref = make_ref(),
+    Num = rand:uniform(Entries),
     Handler ! {read, Ref, Num},
     receive
         {value, Ref, Value} -> Value
     end.
 
-do_write(Num, Handler, Value) ->
+do_write(Entries, Handler, Value) ->
+    Num = rand:uniform(Entries),
     Handler ! {write, Num, Value}.
 
 do_commit(Handler) ->
--- opty/opty.erl
+++ opty/opty.erl
@@ -1,5 +1,5 @@
 -module(opty).
--export([start/4, stop/1]).
+-export([start/5, stop/1]).
 
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
@@ -7,11 +7,13 @@
 %% Writes: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
 
-start(Clients, Entries, SubsetSize, Time) ->
+start(Clients, Entries, Reads, Writes, Time) ->
     register(s, server:start(Entries)),
-    L = startClients(Clients, [], Entries, SubsetSize),
-    io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w SETSZ, ~w WRxTR, DURATION ~w s~n",
-         [Clients, Entries, SubsetSize, Time]),
+    L = startClients(Clients, [], Entries, Reads, Writes),
+    % io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n",
+        %  [Clients, Entries, Reads, Writes, Time]),
+    io:format("~w;~w;~w;~w;~w;",
+        [Clients, Entries, Reads, Writes, Time]),
     timer:sleep(Time*1000),
     stop(L).
 
@@ -24,10 +26,10 @@ stop(L) ->
     io:format("~n"),
     init:stop().
 
-startClients(0, L, _, _) -> L;
-startClients(Clients, L, Entries, SubsetSize) ->
-    Pid = client:start(Clients, Entries, s, SubsetSize),
-    startClients(Clients-1, [Pid|L], Entries, SubsetSize).
+startClients(0, L, _, _, _) -> L;
+startClients(Clients, L, Entries, Reads, Writes) ->
+    Pid = client:start(Clients, Entries, Reads, Writes, s),
+    startClients(Clients-1, [Pid|L], Entries, Reads, Writes).
 
 stopClients([]) ->
     ok;
